236a53799cb84fad1c60e892aa6f0c539e1ace3a
Ansh Sharma <83764908+GithubUser303@users.noreply.github.com>
2026-04-22 21:43:01 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
-from pycaret.classification import load_model, predict_model
+import joblib
+
 app = FastAPI(title="Loan Recommendation API")
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], 
@@ -10,14 +12,20 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-model = load_model('loan_recommendation_model')
+
+model = joblib.load('pure_sklearn_model.pkl')
+
 @app.post("/api/predict")
 async def get_prediction(payload: dict):
     try:
+  
         input_data = payload.get("data", [])
         df = pd.DataFrame(input_data)
-        predictions = predict_model(model, data=df)
-        result = int(predictions['prediction_label'].iloc[0])
+        
+        predictions = model.predict(df)
+        
+        result = int(predictions[0])
+        
         return {"prediction": result}
         
     except Exception as e:
