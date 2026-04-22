a57ab7fc6ba5dd711e64c894246588ec04d39c4c
Ansh Sharma <83764908+GithubUser303@users.noreply.github.com>
2026-04-22 22:23:38 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-model = joblib.load('loan_recommendation_model')
+model = joblib.load('loan_recommendation_model.pkl')
 
 @app.post("/api/predict")
 async def get_prediction(payload: dict):
